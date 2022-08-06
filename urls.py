5e8f3d97ed596627980e5974d0a66b374cfa8e0e
Imron2704 <pythonwithimron@gmail.com>
2022-08-06 14:46:52 +0500

    dsada

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.project_list, name='list'),
-    path('add/', views.ProjectCreateView.as_view(), name='add'),
-    path('<slug:project_slug>/', views.project_detail, name='detail')
-]
